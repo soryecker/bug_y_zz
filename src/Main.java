3e4008894ac18c66eea468a193584012ecf6cb7a
soryecker <2950516647@qq.com>
2021-02-15 23:52:52 +0000

    first

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Tool
 	public static void main(String[] args)
 	{
 		
-		//File f = new File("/storage/emulated/0/");
+		System.out.print("请输入作者作品集URL：");
 		Scanner in = new Scanner(System.in);
 		File f = new File("/storage/emulated/0/图片2.txt");
 		try
@@ -24,12 +24,12 @@ public class Main extends Tool
 			String zz = "(https://www.elitebabes.com/).+(\" title)";
 			//String zz1 = "";
 			String urlt = null;
-			System.out.println("over!!");
-			//String str = url.substring(url.indexOf("<ul class=\"gallery-a b\">"),url.indexOf("<div class='m-pagination"));
+			System.out.println("[*]加载完成!");
+			String str = url.substring(url.indexOf("<ul class=\"gallery-a b\">"),url.indexOf("<div class='m-pagination"));
 			//System.out.println(str);
 			//String g = "(https://k5x5n5g8.ssl.hwcdn.net/content/)[\\w]++(/)[\\w]++(.jpg)";
 			Pattern pat = Pattern.compile(zz);
-			Matcher mat = pat.matcher(url);	
+			Matcher mat = pat.matcher(str);	
 			while(mat.find()){
 				//	System.out.println(mat.group().substring(0,mat.group().length()-7));
 				urlt =doGet(mat.group().substring(0,mat.group().length()-7));
@@ -40,18 +40,6 @@ public class Main extends Tool
 				while(matt.find()){
 					System.out.println(matt.group());
 					write.write(matt.group());write.flush();write.write("\r\n");
-
-					/*Pattern pato = Pattern.compile(g);
-					 Matcher mato = pato.matcher(matt.group());	
-					 while(mato.find()){
-					 //	System.out.println(".");
-					 //if(mato.group().length()==58)	
-					 {
-
-					 //System.out.println(mato.group());
-
-					 }
-					 }*/
 				}
 			}
 			write.close();
